c2099eee4b016cc0b1edc73ca93bb6c4a0d87da6
Kenk <kenghin_lim@hotmail.com>
2022-11-05 21:08:39 +0000

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,50 @@
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-etherscan";
-import "hardhat-gas-reporter";
+import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
-import "@typechain/hardhat";
+import "dotenv/config";
 
-require("dotenv").config();
+import { task } from "hardhat/config";
 
+let ethers = require("ethers");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-//   const accounts = await hre.ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await ethers.getSigners();
 
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
-// task("newwallet", "Generate New Wallet", async (taskArgs, hre) => {
-//   const wallet = hre.ethers.Wallet.createRandom();
-//   console.log("PK: ", wallet._signingKey().privateKey);
-//   console.log("Address: ", wallet.address);
-// });
+task("new:wallet", "Generate New Wallet", async (taskArgs, hre) => {
+  const wallet = ethers.Wallet.createRandom();
+  console.log("PK: ", wallet._signingKey().privateKey);
+  console.log("Address: ", wallet.address);
+});
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+// Setup Default Values
+let PRIVATE_KEY;
+if (process.env.PRIVATE_KEY) {
+  PRIVATE_KEY = process.env.PRIVATE_KEY;
+} else {
+  console.log("⚠️ Please set PRIVATE_KEY in the .env file");
+  PRIVATE_KEY = ethers.Wallet.createRandom()._signingKey().privateKey;
+}
+
+let PRIVATE_KEY_TESTNET;
+if (process.env.PRIVATE_KEY_TESTNET) {
+  PRIVATE_KEY_TESTNET = process.env.PRIVATE_KEY_TESTNET;
+} else {
+  console.log("⚠️ Please set PRIVATE_KEY_TESTNET in the .env file");
+  PRIVATE_KEY_TESTNET = ethers.Wallet.createRandom()._signingKey().privateKey;
+}
+
+if (!process.env.INFURA_API_KEY) {
+  console.log("⚠️ Please set INFURA_API_KEY in the .env file");
+}
+
+if (!process.env.ETHERSCAN_API_KEY) {
+  console.log("⚠️ Please set ETHERSCAN_API_KEY in the .env file");
+}
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -34,56 +55,95 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545",
       saveDeployments: true,
-      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
+      // accounts: [PRIVATE_KEY],
     },
     hardhat: {
-      chainId: 1337,
+      // TODO: Add snapshot block
       // forking: {
-      //   url: "https://rpc.vvs.finance",
-      //   blockNumber: 1876666
+      //   url: process.env.ALCHEMY_PROVIDER_MAINNET,
+      //   block: 0,
       // },
+      blockGasLimit: 10000000000,
+      mining: {
+        auto: true,
+      },
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      chainId: 1,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
-      // mining: {
-      //   auto: true,
-      // },
     },
-    cronos_testnet: {
-      url: `https://cronos-testnet-3.crypto.org:8545/`,
-      chainId: 338,
-      accounts: [process.env.PRIVATE_KEY],
-      gas: 21000000,
-      gasPrice: 5000000000000,
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      chainId: 4,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
     },
-    cronos_mainnet: {
-      url: `https://rpc.vvs.finance`,
-      chainId: 25,
-      accounts: [process.env.PRIVATE_KEY],
-      gas: 2100000,
-      gasPrice: 5000000000000,
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      chainId: 5,
+      accounts: [PRIVATE_KEY_TESTNET],
+      saveDeployments: true,
     },
-    // mainnet: {
-    //   url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    //   chainId: 1,
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
-    // rinkeby: {
-    //   url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    //   chainId: 4,
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
-    // matic: {
-    //   url: "https://polygon-rpc.com/",
-    //   chainId: 137,
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
-    // mumbai: {
-    //   url: "https://rpc-mumbai.matic.today",
-    //   chainId: 80001,
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
+    matic: {
+      url: "https://polygon-rpc.com/",
+      chainId: 137,
+      accounts: [PRIVATE_KEY],
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      chainId: 80001,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+    },
+    optimism_mainnet: {
+      url: "https://mainnet.optimism.io",
+      chainId: 10,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+    },
+    optimism_testnet: {
+      url: "https://goerli.optimism.io/",
+      chainId: 420,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+    },
+    arbitrum_mainnet: {
+      url: "https://arb1.arbitrum.io/rpc",
+      chainId: 42161,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+    },
+    arbitrum_testnet: {
+      url: "https://rinkeby.arbitrum.io/rpc",
+      chainId: 421611,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+    },
+    klaytn_mainnet: {
+      url: "https://klaytn04.fandom.finance",
+      chainId: 8217,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+    },
+    klaytn_testnet: {
+      url: "https://public-node-api.klaytnapi.com/v1/baobab",
+      chainId: 1001,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+    },
   },
   solidity: {
     compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+          },
+        },
+      },
       {
         version: "0.8.11",
         settings: {
@@ -92,15 +152,19 @@ module.exports = {
           },
         },
       },
-     
+      {
+        version: "0.7.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+          },
+        },
+      },
     ],
   },
   namedAccounts: {
     deployer: {
       default: 0, // here this will by default take the first account as deployer
-    },
-    treasury: {
-      default: 1, // here this will by default take the first account as deployer
     },
   },
   etherscan: {
@@ -117,8 +181,12 @@ module.exports = {
   mocha: {
     timeout: 2000000000,
   },
-  typechain:{
-    outDir:"typechain",
-    target:"ethers-v5",
-  }
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+  gasReporter: {
+    enabled: true,
+    gasPrice: 100,
+  },
 };
